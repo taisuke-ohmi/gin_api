f723b25f5de5525ee55abdb8bc9875dfd7459f4d
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-09-23 15:13:36 +0900

    User型を作成

--- user/user.go
+++ user/user.go
@@ -6,9 +6,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type User struct {
+	Name     string `form:"name" binding:"required"`
+	Password string `form:"password" binding:"required"`
+}
+
 func UserCreate(c *gin.Context) {
+	var user User
+
+	if err := c.ShouldBind(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
 	name := c.PostForm("name")
+	password := c.PostForm("password")
 	c.JSON(http.StatusOK, gin.H{
-		"name": name,
+		"name":     name,
+		"password": password,
 	})
 }
