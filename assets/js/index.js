156802e2612ba19a6822105bb9831aaf7928ec15
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-09-23 15:14:04 +0900

    パスワードを処理できるように

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@ window.onload = function() {
   var button = document.getElementById('send');
   var result = document.getElementById('result');
   var username = document.getElementById("name");
+  var password = document.getElementById("password");
   var form = document.getElementsByTagName('form')[0];
   form.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -11,15 +12,15 @@ window.onload = function() {
     var req = new XMLHttpRequest();
     req.onreadystatechange = function() {
       if (req.readyState == 4 && req.status == 200) {
-        var name = JSON.parse(req.responseText).name;
+        var obj = JSON.parse(req.responseText);
         var newEl = document.createElement('li');
-        newEl.innerHTML = name;
+        newEl.innerHTML = "名前:" + obj.name + " パスワード:" + obj.password;
         result.appendChild(newEl);
       }
     };
 
     req.open('POST', 'user/create', true);
     req.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
-    req.send('name=' + encodeURIComponent(username.value));
+    req.send('name=' + encodeURIComponent(username.value) + '&password=' + encodeURIComponent(password.value));
   }, false);
 };
