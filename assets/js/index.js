7e4af1473ab081ac39fcd8e138e8447e78d10dac
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-09-24 16:49:54 +0900

    jqueryでajax処理

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,21 @@
 window.onload = function() {
-  var button = document.getElementById('send');
-  var result = document.getElementById('result');
-  var username = document.getElementById("name");
-  var password = document.getElementById("password");
-  var form = document.getElementsByTagName('form')[0];
-  form.addEventListener('submit', function(e) {
+  var result = $('#result');
+
+  $('.js-form').submit(function(e) {
     e.preventDefault();
+    var $form = $(this);
+
+    $.ajax({
+      url: $form.attr('action'),
+      type: $form.attr('method'),
+      data: $form.serialize(),
+      dataType: 'json',
+    }).done((data) => {
+      $('<li>')
+        .html("名前:" + data.name + " パスワード:" + data.password)
+        .appendTo(result);
+    }).fail((data) => {
+      console.log(data);
+    });
   });
-
-  button.addEventListener("click", function() {
-    var req = new XMLHttpRequest();
-    req.onreadystatechange = function() {
-      if (req.readyState == 4 && req.status == 200) {
-        var obj = JSON.parse(req.responseText);
-        var newEl = document.createElement('li');
-        newEl.innerHTML = "名前:" + obj.name + " パスワード:" + obj.password;
-        result.appendChild(newEl);
-      }
-    };
-
-    req.open('POST', 'user/create', true);
-    req.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
-    req.send('name=' + encodeURIComponent(username.value) + '&password=' + encodeURIComponent(password.value));
-  }, false);
 };
